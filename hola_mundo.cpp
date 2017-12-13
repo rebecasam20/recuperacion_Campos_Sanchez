66a1bbffac3eb4c45ae8419bc42dc7d7c1cd1dc0
rebecasam20 <samai2197@hotmail.com>
2017-12-12 18:48:12 -0600

    funcion suma

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
+	int a,b;
 	cout<<"HOlA MUNDO";
+	cout<<"\nPrimer numero ";
+	cin>>a;
+	cout<<"\nSegundo numero ";
+	cin>>b;
+	cout<<"resultado= "<<a+b;
 	return 0;
 
 } 
