c23af82f9ceddcbb45e2533c8b134496694c16f9
rebecasam20 <samai2197@hotmail.com>
2017-12-12 18:56:26 -0600

    factorial

--- hola_mundo.cpp
+++ hola_mundo.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
 using namespace std;
+long double factorial(int);
 int main(){
-	int a,b;
+	int a,b,n;
 	cout<<"HOlA MUNDO";
 	cout<<"\nPrimer numero ";
 	cin>>a;
 	cout<<"\nSegundo numero ";
 	cin>>b;
 	cout<<"resultado= "<<a+b;
+	
+	cout << "\n\nFactorial \nIntroduzca numero: ";
+    cin >> n;
+    cout << "\nfactorial: " << factorial(n) << endl;
+    
 	return 0;
-
+} 
+long double factorial(int n)
+{
+    long double fact;
+    if (n==0)
+        return 1;
+    else
+         return n*factorial(n-1);
 } 
